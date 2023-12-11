5f2e106c7eb89e905f160a45c87324edca9a9dd0
Sirigiri Madan Mohan Reddy <121308565+SirigiriMadanMohanReddy@users.noreply.github.com>
2023-12-11 10:12:32 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const apiKey = 'YOUR_API_KEY';
+const videoId = 'YOUR_VIDEO_ID';
+function onYouTubeIframeAPIReady() {
+    const player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: videoId,
+        playerVars: {
+            'autoplay': 1,
+            'controls': 1,
+            'showinfo': 0,
+            'rel': 0,
+            'modestbranding': 1,
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+function onPlayerReady(event) {
+    event.target.playVideo();
+}function onPlayerStateChange(event) {
+
+}
